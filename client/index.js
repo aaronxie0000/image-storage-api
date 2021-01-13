082c4f96a16154c29d03dd96bac51f7333cf9bf5
aaron2024 <xieaaron@hotmail.com>
2021-01-12 21:18:46 -0800

    functioning multer ver

---
+++ client/index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+
+
+app.get('/', express.static('./'), (req, res)=>{
+    res.end();
+  })
+  
+app.listen(3003, () => console.log("listening on 3003"));
+
+
--- server/app.js
+++ server/app.js
@@ -1,12 +1,12 @@
-// const fs = require("fs/promises");
 const path = require("path");
+const { v1: uuidv1 } = require("uuid");
+
 const express = require("express");
 const app = express();
 const cors = require("cors");
 const multer = require("multer");
-const { v1: uuidv1 } = require("uuid");
 
-//disk storage is just so can add more customization to filename and dest
+
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, './images')
@@ -24,34 +24,21 @@ app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb" }));
 
-function fileNameParse(req, res, next) {
-  console.log(req.file);
-  next();
-}
 
 
-
-app.post("/images", upload.single("imageOrigin"),fileNameParse, async (req, res) => {
-    // const rawSrc = req.body.imgSrc;
-    // const fileName = req.body.fileName;
-
-    // console.log(req.body.tags);
-
-    // const ext = rawSrc.substring(
-    //   rawSrc.indexOf("/") + 1,
-    //   rawSrc.indexOf(";base64")
-    // );
-    // const fileType = rawSrc.substring("data:".length, rawSrc.indexOf("/"));
-
-    // const regexTarget = new RegExp(`^data:${fileType}\/${ext};base64,`, "gi");
-    // const base64Data = rawSrc.replace(regexTarget, "");
-
-    // const fileID = uuidv1();
-
-    // await fs.writeFile(`./images/${fileID}.${ext}`, base64Data, "base64");
-
+app.post("/images", upload.single("imageOrigin"), async (req, res) => {
+    //req.file has all info 
+    // req.body.imageTags (text content of the form is in req.body)
+    console.log(req.body.imageTags);
     res.end();
   }
 );
 
+//if was hosting on same server;
+// app.get('/', express.static('./../client'), (req, res)=>{
+//   res.end();
+// })
+
 app.listen(3000, () => console.log("listening on 3000"));
+
+
